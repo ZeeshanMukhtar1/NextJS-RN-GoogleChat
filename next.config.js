dbee2dad3e472901a50c9f6c490fb308f56faff4
Zeeshan Mukhtar <91063160+ZeeshanMukhtar1@users.noreply.github.com>
2023-11-14 18:18:32 -0800

    📦 used styles preprocessor

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+const withLess = require('next-with-less');
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  /* other next.js config options here */
+};
 
-module.exports = nextConfig
+module.exports = withLess(nextConfig);
